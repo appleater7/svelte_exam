6821879f3f1cff42a4fafb13496a29a9e11ea83e
appleater <appleater7@gmail.com>
2020-06-07 21:06:46 +0900

    init

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Header from './components/Header.svelte'
+	import Footer from './components/Footer.svelte'
+	import Tabs from './shared/Tabs.svelte'
+
+	// tabs
+	let items = ['Current Polls', 'Add New Poll'];
+	let activeItem = 'Current Polls';
+</script>
+
+<Header/>
+<main>
+	<Tabs {activeItem} {items}/>
+</main>
+<Footer/>
+
+<style>
+	main{
+		max-width: 960px;
+		margin: 40px auto;
+	}
+</style>
